0a6a73b6d49bf282c341e9d86e1a220a3d4eadba
abhay pratp <abhay27x06@gmail.com>
2025-01-12 17:17:05 +0530

    first

---
+++ src/main/java/com/springT/First/FirstApplication.java
@@ -0,0 +1,14 @@
+package com.springT.First;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone");
+		SpringApplication.run(FirstApplication.class, args);
+	}
+
+}
