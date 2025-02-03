328b16153d8900450e3a7a7804c25e04f342234a
abhay pratp <abhay27x06@gmail.com>
2025-02-04 01:42:07 +0530

    another

--- src/main/java/com/springT/First/config/JwtFilter.java
+++ src/main/java/com/springT/First/config/JwtFilter.java
@@ -39,7 +39,7 @@ public class JwtFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }
-            filterChain.doFilter(request, response);
         }
+        filterChain.doFilter(request, response);
     }
 }
